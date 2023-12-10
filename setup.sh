72eb8b031db7c1bcce69a6bd3b7fef8043b3137d
Atul93 <54507991+Atuls-git@users.noreply.github.com>
2023-12-10 16:52:33 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if script is running as root user
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. Please use 'sudo' or run as root."
+    exit 1
+fi
+
+# Install Python3-pip
+apt install python3-pip -y
+
+# Install Git
+apt install git -y
+
+# Install other requirements to run the script
+pip install opencv-python imageio
