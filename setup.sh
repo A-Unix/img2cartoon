0337f607a7fd592f329d0f6fb6bc15a58b57e73f
Atul93 <54507991+Atuls-git@users.noreply.github.com>
2023-12-10 17:20:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,7 @@ apt install git -y
 
 # Install other requirements to run the script
 pip install opencv-python imageio
+
+# Additional requirements for Imageo for error handling
+pip install imageio[ffmpeg]
+pip install imageio[pyav]
